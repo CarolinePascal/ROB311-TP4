917d76d82a1dd25aa9ca4ff8f30753283828399e
sarah <sarah_curtit@yahoo.fr>
2019-10-13 22:42:04 +0200

    small modifications

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import numpy as np
 import csv
 import random as rd
 import time
+import pickle
 
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 
 def load_data(PATH):
+    """ load data and corresponding labels from csv file """
     with open(PATH) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         images = []
@@ -23,6 +25,15 @@ def load_data(PATH):
     return(np.array(labels),np.array(images))
 
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.RdPu):
+    """ plot a fancy confusion matrix """
+    
+    fig, ax = plt.subplots(figsize=(15, 12))
+    #Setting the axes with labels
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           ylabel='True label',
+           xlabel='Predicted label')
+    
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
@@ -30,10 +41,20 @@ def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.RdPu):
     plt.xticks(tick_marks, numbers)
     plt.yticks(tick_marks, numbers)
     plt.tight_layout()
+    
+    thresh = cm.max() / 4.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, np.round(cm[i, j], 4),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+            
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 
 def plot_labels_repartition(labels,labels_array,title = "Histogram",cmap=plt.cm.RdPu):
+    """ plot an histogram of label repartition """
+   
     repartition = np.empty(len(labels))
     for i,label in enumerate(labels):
         repartition[i] = np.count_nonzero(labels_array == label)
@@ -44,27 +65,33 @@ def plot_labels_repartition(labels,labels_array,title = "Histogram",cmap=plt.cm.
     plt.ylabel('Occurences')
     plt.xlabel('Labels')
     
+    
+    
+# train and display train and test datasets
+
 start = time.time() 
-labels_train,images_train = load_data('mnist_train.csv')
+labels_train,images_train = load_data('drive/My Drive/Colab Notebooks/TP4_learning_for_robotics/mnist_train.csv')
 end = time.time()
 print("-- TRAIN DATA LOADED --")
 print("-- LOAD TIME = "+str(end-start)+" s --")
 
 plt.figure()
-plot_labels_repartition(numbers,labels_train)
+plot_labels_repartition(numbers,labels_train, "train set")
 plt.show()
 
 start = time.time()
-labels_test,images_test = load_data('mnist_test.csv')
+labels_test ,images_test = load_data('drive/My Drive/Colab Notebooks/TP4_learning_for_robotics/mnist_test.csv')
 end = time.time()
 print("-- TEST DATA LOADED --")
 print("-- LOAD TIME = "+str(end-start)+" s --")
 
 plt.figure()
-plot_labels_repartition(numbers,labels_test)
+plot_labels_repartition(numbers,labels_test, "test set")
 plt.show()
 
-clf = svm.SVC(gamma = 'scale')
+# train SVC model 
+
+clf = svm.SVC(gamma = 'scale', verbose=1)
 print("-- MODEL PARAMETERS --")
 print(clf)
 
@@ -74,6 +101,12 @@ end = time.time()
 
 print("-- TRAINING OK --")
 print("-- TRAINING TIME = "+str(end-start)+" s --")
+
+#save model and display results
+
+with open('drive/My Drive/Colab Notebooks/TP4_learning_for_robotics/clf', 'wb') as outfile:
+    pickle.dump(clf, outfile)
+print("-- MODEL SAVED with pickle in file clf --")
 
 start = time.time()
 labels_predict = clf.predict(images_test)
@@ -96,5 +129,3 @@ plt.show()
 plt.figure()
 plot_confusion_matrix(normalized_martix, title='Normalized confusion matrix')
 plt.show()
-
-
